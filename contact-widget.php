ead8e22e0b07cd3e4814b3600293cbf71a15a753
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-14 10:06:04 +0300

    Plugin header

---
+++ contact-widget.php
@@ -0,0 +1,11 @@
+/*
+ * Plugin Name:       Contact Widget
+ * Plugin URI:        https://weband.bg
+ * Description:       I love to practise <3
+ * Version:           1.0
+ * Requires PHP:      7.2
+ * Author:            Antonio Pavlov
+ * Author URI:        https://apavlov.bg
+ * License:           GPL v2 or later
+ * Text Domain:       contact-widget
+ */
